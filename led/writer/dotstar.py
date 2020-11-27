a620cfce5a6b628f2fc57f4acda790f9fc58b037
Robin Deits <robin.deits@gmail.com>
2020-11-27 00:04:22 -0500

    don't autowrite

--- led/writer/dotstar.py
+++ led/writer/dotstar.py
@@ -11,7 +11,10 @@ class DotstarWriter(ledctl.WriterNode):
 
     def __init__(self, **kwargs):
         ledctl.WriterNode.__init__(self, **kwargs)
-        self.strip = dotstar.DotStar(board.SCK, board.MOSI, self.num_lights, brightness=0.2)
+        self.strip = dotstar.DotStar(board.SCK, board.MOSI,
+            self.num_lights,
+            brightness=0.2,
+            auto_write=False)
         # self.strip = Adafruit_DotStar(self.num_lights)  # Use SPI (pins 10=MOSI, 11=SCLK)
         # self.strip.begin()           # Initialize pins for output
         # self.strip.setBrightness(128)
@@ -21,7 +24,7 @@ class DotstarWriter(ledctl.WriterNode):
         for (i, pixel) in enumerate(frame):
             self.strip[i] = (pixel[0], pixel[1], pixel[2])
             # self.strip.setPixelColor(i, pixel[1], pixel[0], pixel[2])
-        # self.strip.show()
+        self.strip.show()
 
 if __name__ == '__main__':
     DotstarWriter(**json.loads(sys.argv[1])).start()
