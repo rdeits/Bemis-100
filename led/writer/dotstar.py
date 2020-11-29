256bf9a2e4b9774e3d6fa9b9559d53bc45f3aa63
Robin Deits <robin.deits@gmail.com>
2020-11-29 14:01:14 -0500

    fix typo

--- led/writer/dotstar.py
+++ led/writer/dotstar.py
@@ -3,7 +3,7 @@ from __future__ import division
 import led.ledctl as ledctl
 import sys
 import json
-import board
+import board  # provided by adafruit-circuitpython-dotstar
 import busio
 
 
@@ -14,7 +14,7 @@ class DotstarWriter(ledctl.WriterNode):
         self.spi = busio.SPI(board.SCK, MOSI=board.MOSI)
         while not self.spi.try_lock():
             pass
-        spi.configure(baudrate=4000000)
+        self.spi.configure(baudrate=4000000)
 
         # self.strip = dotstar.DotStar(board.SCK, board.MOSI,
         #     self.num_lights,
