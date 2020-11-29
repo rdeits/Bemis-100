53845d26569ed6385284502326b586e29ec4651f
Robin Deits <robin.deits@gmail.com>
2020-11-29 14:00:12 -0500

    new low-level dotstar driver

--- led/writer/dotstar.py
+++ led/writer/dotstar.py
@@ -4,27 +4,42 @@ import led.ledctl as ledctl
 import sys
 import json
 import board
-import adafruit_dotstar as dotstar
+import busio
 
 
 class DotstarWriter(ledctl.WriterNode):
 
     def __init__(self, **kwargs):
         ledctl.WriterNode.__init__(self, **kwargs)
-        self.strip = dotstar.DotStar(board.SCK, board.MOSI,
-            self.num_lights,
-            brightness=0.2,
-            auto_write=False)
+        self.spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+        while not self.spi.try_lock():
+            pass
+        spi.configure(baudrate=4000000)
+
+        # self.strip = dotstar.DotStar(board.SCK, board.MOSI,
+        #     self.num_lights,
+        #     brightness=0.2,
+        #     auto_write=False)
         # self.strip = Adafruit_DotStar(self.num_lights)  # Use SPI (pins 10=MOSI, 11=SCLK)
         # self.strip.begin()           # Initialize pins for output
         # self.strip.setBrightness(128)
         self.blank()
 
     def draw_frame(self, frame):
-        for (i, pixel) in enumerate(frame):
-            self.strip[i] = (pixel[0], pixel[1], pixel[2])
-            # self.strip.setPixelColor(i, pixel[1], pixel[0], pixel[2])
-        self.strip.show()
+        header = bytearray(b'\x00') * 4
+        trailer_size = self.num_lights // 16
+        if self.num_lights % 16 != 0:
+            trailer_size += 1
+        trailer = bytearray(b'\xff') * trailer_size
+
+        permutation = (2, 1, 0)
+        data = np.hstack(np.full((self.num_lights, 1), 255, dtype=np.uint8),
+                         frame[:, p].astype(np.uint8)).tobytes()
+        # for (i, pixel) in enumerate(frame):
+        #     self.strip[i] = (pixel[0], pixel[1], pixel[2])
+        #     # self.strip.setPixelColor(i, pixel[1], pixel[0], pixel[2])
+        # self.strip.show()
+        self.spi.write(header + data + trailer)
 
 if __name__ == '__main__':
     DotstarWriter(**json.loads(sys.argv[1])).start()
